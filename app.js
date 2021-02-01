ae2922efe01ac22510d07ab4f0e1fe188722fc95
mark-rahal <mgr0904@gmail.com>
2021-02-01 13:35:43 -0800

    Added auto shutdown if no players online

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var EC2 = require('aws-sdk/clients/ec2');
+var metadata = require('./metadata');
+var minecraftPing = require('minecraft-ping')
 
 var indexRouter = require('./routes/index');
 
@@ -35,5 +38,27 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
+
+function stopServerIfEmpty() {
+  minecraftPing.ping_fe01fa({ host: metadata.serverIP, port: 25565 }, (err, response) => {
+    if (err) {
+      return;
+    }
+    if (response.playersOnline === 0) {
+      var ec2 = new EC2({
+        apiVersion: '2016-04-01',
+        region: 'us-west-1'
+      });
+
+      ec2.stopInstances({ InstanceIds: [metadata.instanceId] }).promise().then(function(data) {
+        console.log("No players online, stopping server.");
+      }).catch(function(err) {
+        console.log(err.message);
+      });
+    }
+  })
+}
+
+setInterval(stopServerIfEmpty, metadata.shutdownCheckInterval);
 
 module.exports = app;
--- metadata.js
+++ metadata.js
@@ -1,3 +1,5 @@
 module.exports = {
-  instanceId: process.env.INSTANCE_ID
+  instanceId: process.env.INSTANCE_ID,
+  serverIP: process.env.SERVER_IP,
+  shutdownCheckInterval: process.env.SHUTDOWN_CHECK_INTERVAL
 }
